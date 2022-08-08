a2f761a522affdb9544ca4c510447967e3eff8d3
Pranav <pranav.mishra@goodworklabs.com>
2022-08-08 14:24:36 +0530

    firebase otp auth

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,128 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { Button, ButtonGroup } from "@chakra-ui/react";
-function App() {
+import React, { useEffect, useState } from "react";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// import "firebase/auth";
+
+import { BrowserRouter as Router, Route, Switch } from "react";
+
+//components
+import SignIn from "./components/SignIn";
+import Home from "./components/HomePage";
+
+const App = () => {
+  const [viewOtpForm, setViewOtpForm] = useState(false);
+  const [user, setUser] = useState(false);
+  const firebaseConfig = {
+    apiKey: "AIzaSyCkTWTxlqXf9JOq4crEY-URmDW0ME_-qkA",
+    authDomain: "heftr-14dea.firebaseapp.com",
+    projectId: "heftr-14dea",
+    storageBucket: "heftr-14dea.appspot.com",
+    messagingSenderId: "527542522695",
+    appId: "1:527542522695:web:44a3d98296000f2dd0b262",
+    measurementId: "G-805ZTXMBNN"
+  };
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+
+  useEffect(() => {
+    window.recaptchaVerifier = new app.auth.RecaptchaVerifier(
+      "recaptcha-container",
+      {
+        size: "invisible",
+        callback: function (response) {
+          console.log("Captcha Resolved");
+          this.onSignInSubmit();
+        },
+        defaultCountry: "IN",
+      }
+    );
+  }, []);
+
+  if (!app.length) {
+    initializeApp(firebaseConfig);
+  } else {
+    app(); // if already initialized, use that one
+  }
+
+  const auth = app.auth();
+
+  auth.onAuthStateChanged((user) => {
+    if (user) {
+      setUser(user);
+    }
+  });
+
+  const loginSubmit = (e) => {
+    e.preventDefault();
+
+    let phone_number = "+91" + e.target.phone.value;
+    const appVerifier = window.recaptchaVerifier;
+
+    auth
+      .signInWithPhoneNumber(phone_number, appVerifier)
+      .then((confirmationResult) => {
+        // SMS sent. Prompt user to type the code from the message, then sign the
+        // user in with confirmationResult.confirm(code).
+        console.log("otp sent");
+        setViewOtpForm(true);
+        window.confirmationResult = confirmationResult;
+        // ...
+      })
+      .catch((error) => {
+        // Error; SMS not sent
+        // ...
+        alert(error.message);
+      });
+  };
+
+  const otpSubmit = (e) => {
+    e.preventDefault();
+
+    let opt_number = e.target.otp_value.value;
+
+    window.confirmationResult
+      .confirm(opt_number)
+      .then((confirmationResult) => {
+        console.log(confirmationResult);
+        console.log("success");
+        window.open("/", "_self");
+      })
+      .catch((error) => {
+        // User couldn't sign in (bad verification code?)
+        alert(error.message);
+      });
+  };
+
+  const signOut = () => {
+    app
+      .auth()
+      .signOut()
+      .then(() => {
+        window.open("/signin", "_self");
+      })
+      .catch((error) => {
+        // An error happened.
+        console.log(error);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button colorScheme="blue">Button</Button>
-      </header>
-    </div>
+    <Router>
+      <div id="recaptcha-container"></div>
+      <Switch>
+        <Route path="/" exact>
+          <Home signOut={signOut} user={user} />
+        </Route>
+        <Route path="/signin" exact>
+          <SignIn
+            loginSubmit={loginSubmit}
+            otpSubmit={otpSubmit}
+            viewOtpForm={viewOtpForm}
+          />
+        </Route>
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
