ab322eaeec96dfb0be75553a508a91731acf11ca
Pranav <pranav.mishra@goodworklabs.com>
2022-08-27 00:04:48 +0530

    phone OTP auth verification
    Added OTP

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,29 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import { Provider } from "react-redux";
-import { configureStore } from "./store";
-import reportWebVitals from "./reportWebVitals";
-import { ChakraProvider } from "@chakra-ui/react";
-import { BrowserRouter } from "react-router-dom";
-import HomePage from "./components/HomePage";
-const root = ReactDOM.createRoot(document.getElementById("root"));
-const store = configureStore();
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <ChakraProvider>
-          <App />
-          <HomePage />
-        </ChakraProvider>
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+import { ChakraProvider } from "@chakra-ui/react";
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+// import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <ChakraProvider>
+      <App/>
+      </ChakraProvider>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
