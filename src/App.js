d6a70e251daeb13950b7be508e6f231015de4c77
Shubham <shubhamdv19@gmail.com>
2022-08-30 23:39:59 +0530

    login ui fixes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import { Container, Row, Col } from "react-bootstrap";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
@@ -8,26 +7,19 @@ import ProtectedRoute from "./components/ProtectedRoute";
 import { UserAuthContextProvider } from "./context/UserAuthContext";
 function App() {
   return (
-    <Container style={{ width: "400px" }}>
-      <Row>
-        <Col>
-          <UserAuthContextProvider>
-            <Routes>
-              <Route
-                path="/home"
-                element={
-                  <ProtectedRoute>
-                    <Home />
-                  </ProtectedRoute>
-                }
-              />
-              <Route path="/" element={<PhoneSignUp />} />
-            </Routes>
-          </UserAuthContextProvider>
-        </Col>
-      </Row>
-    </Container>
-
+    <UserAuthContextProvider>
+      <Routes>
+        <Route
+          path="/home"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/" element={<PhoneSignUp />} />
+      </Routes>
+    </UserAuthContextProvider>
   );
 }
 
--- src/components/PhoneSignUp.js
+++ src/components/PhoneSignUp.js
@@ -2,20 +2,19 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Form, Alert } from "react-bootstrap";
 import "react-phone-number-input/style.css";
-import { 
+import {
   Container,
   Button,
   Box,
-  Text,
-  Image,
-  Badge,
   InputLeftAddon,
   Input,
   InputGroup,
   HStack,
   VStack,
-} from '@chakra-ui/react'
-import PhoneInput from "react-phone-number-input";
+  Heading,
+  useBreakpointValue,
+  Stack,
+} from "@chakra-ui/react";
 import { useUserAuth } from "../context/UserAuthContext";
 
 const PhoneSignUp = () => {
@@ -31,7 +30,7 @@ const PhoneSignUp = () => {
     e.preventDefault();
     console.log(number);
     setError("");
-    let val = "+91"+number;
+    let val = "+91" + number;
     // setNumber(prev=>val)
     if (number === "" || number === undefined)
       return setError("Please enter a valid phone number!");
@@ -47,56 +46,86 @@ const PhoneSignUp = () => {
   const verifyOtp = async (e) => {
     e.preventDefault();
     setError("");
-    console.log("hi")
+    console.log("hi");
     if (otp === "" || otp === null) return;
     try {
       await result.confirm(otp);
       navigate("/home");
-      console.log("verify OTP")
+      console.log("verify OTP");
     } catch (err) {
       setError(err.message);
     }
   };
 
-  const [toggle, setToggle] = useState(false)
-  return(
-    <Box maxW='md' borderWidth='1px' borderRadius='lg' justifyContent={"space-around"} alignItems={"center"}>
-      {error && <Alert variant="danger">{error}</Alert>}
-      <Form onSubmit={getOtp} 
-      // style={{ display: !flag ? "block" : "none" }}
+  const [toggle, setToggle] = useState(false);
+  return (
+    <section
+      style={{
+        display: "flex",
+        height: "100vh",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Container
+        centerContent
+        py={{ base: "12", md: "24" }}
+        px={{ base: "0", sm: "8" }}
       >
-        <Text>Login With Phone Number</Text>
-        <Box padding={"10px"}>
-          <InputGroup>
-            <InputLeftAddon children='+91' />
-            <Input type='tel' placeholder='phone number' value={number} onChange={(e)=>setNumber(e.target.value)} />
-          </InputGroup>
-          <VStack>
-          <div id="recaptcha-container"></div>
-          </VStack>
-          <HStack>
-          {toggle?
-            <HStack justifyContent={"center"} alignItems={"center"} p={"1.5"} h={"100%"}>
-              <Input placeholder="Enter OTP" maxWidth={"40%"}
-                onChange={(e) => setOtp(e.target.value)}>
-              </Input>
-              <Button colorScheme='teal' size='md' p={"1"} onClick={verifyOtp}>
-                Submit
-              </Button>
-            </HStack>
-            :
-            <Button colorScheme='teal' size='md' p={"1"} onClick={(e)=>{
-              getOtp(e);
-              setToggle(prev=>!prev)}}>
-              GET OTP
-            </Button>
-        }
-          </HStack>
+        <Heading size={useBreakpointValue({ base: "xs", md: "sm" })}>
+          Log in With Phone Number
+        </Heading>
+        <Box maxW="md" borderWidth="1px" borderRadius="lg">
+          {error && <Alert variant="danger">{error}</Alert>}
+          <Form
+            onSubmit={getOtp}
+            // style={{ display: !flag ? "block" : "none" }}
+          >
+            <Box padding={"20px"}>
+              <InputGroup>
+                <InputLeftAddon children="+91" />
+                <Input
+                  type="tel"
+                  placeholder="phone number"
+                  value={number}
+                  onChange={(e) => setNumber(e.target.value)}
+                />
+              </InputGroup>
+              <VStack>
+                <div id="recaptcha-container"></div>
+              </VStack>
+              <HStack>
+                {toggle ? (
+                  <VStack p={"1.5"} h={"100%"} align="stretch">
+                    <Input
+                      placeholder="Enter OTP"
+                      maxWidth={"40%"}
+                      onChange={(e) => setOtp(e.target.value)}
+                    ></Input>
+                    <Button colorScheme="teal" size="lg" onClick={verifyOtp}>
+                      Submit
+                    </Button>
+                  </VStack>
+                ) : (
+                  <Button
+                    colorScheme="teal"
+                    mt="10"
+                    size="lg"
+                    onClick={(e) => {
+                      getOtp(e);
+                      setToggle((prev) => !prev);
+                    }}
+                  >
+                    Get OTP
+                  </Button>
+                )}
+              </HStack>
+            </Box>
+          </Form>
         </Box>
-      </Form>
-    </Box>
-
-  )
+      </Container>
+    </section>
+  );
 };
 
 export default PhoneSignUp;
