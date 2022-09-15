aeb5bf274cad1f40e1aa60dc3e7271c29dac08dd
Pranav <pranavmishra559@gmail.com>
2022-09-15 08:53:23 +0530

    image render fixed

--- src/components/SingleProduct/index.js
+++ src/components/SingleProduct/index.js
@@ -2,10 +2,15 @@ import React, { useState, useEffect } from "react";
 import sanityClient from "../../client";
 import { useParams } from "react-router-dom";
 import imageURLBuilder from "@sanity/image-url";
+import { m } from "framer-motion";
 
 const builder = imageURLBuilder(sanityClient);
 function urlFor(source) {
-  return builder.image(source);
+  return builder
+    .image(source)
+    .auto('format')
+    .fit('max')
+    .url()
 }
 const SingleProduct = () => {
   const [productDetail, setProductDetail] = useState(null);
@@ -41,11 +46,14 @@ const SingleProduct = () => {
         "authorImage": author->image 
     }`
       )
-      .then((data) => setProductDetail(data[0]))
-      .catch((error) => console.log(error));
+      .then((data) =>{
+        console.log("slug data",data)
+        setProductDetail(data[0])})
+      .catch((error) => console.log("error",error));
   }, [slug]);
-  return (
-    <section className="bg-white dark:bg-gray-900">
+  console.log("detailed",productDetail)
+  return (productDetail &&
+      <section className="bg-white dark:bg-gray-900">
       <div className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
         <div className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
           <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
@@ -58,27 +66,33 @@ const SingleProduct = () => {
             solvers. Small enough to be simple and quick.
           </p> */}
         </div>
-        {/* <div className="grid grid-cols-2 gap-4 mt-8">
+        <div className="grid grid-cols-2 gap-4 mt-8">
           <img
             className="w-full rounded-lg"
             src={
-              productDetail &&
-              productDetail.images &&
-              productDetail.images[0].asset._ref
+              urlFor(productDetail &&
+                productDetail?.images &&
+                productDetail?.images[0]?.asset._ref)
             }
             alt="office content 1"
           />
-          {JSON.stringify(productDetail?.images[0].asset._ref)}
           <img
             className="mt-4 w-full lg:mt-10 rounded-lg"
-            src={urlFor(
-              productDetail && productDetail.images[1].asset._ref
-            ).url()}
+            src={urlFor(productDetail &&
+              productDetail?.images &&
+              productDetail?.images[2]?.asset._ref)}
             alt="office content 2"
           />
-        </div> */}
+          <img
+            className="mt-4 w-full lg:mt-10 rounded-lg"
+            src={urlFor(productDetail &&
+              productDetail?.images &&
+              productDetail?.images[1]?.asset._ref)}
+            alt="office content 2"
+          />
+        </div>
       </div>
-    </section>
+      </section>
     //   <section class="bg-white dark:bg-gray-900">
     //     <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
     //       <div class="max-w-screen-lg text-gray-500 sm:text-lg dark:text-gray-400">
