a65c71ef8b7586ce17844b1ae82c0a087281f720
shubham <33717931+shubhamdsm@users.noreply.github.com>
2022-11-05 14:00:57 +0530

    added dummy creds

--- src/components/PhoneSignUp.js
+++ src/components/PhoneSignUp.js
@@ -1,15 +1,18 @@
 import React, { useState } from "react";
+import Loader from "./Loader";
 import { useNavigate } from "react-router-dom";
 import "react-phone-number-input/style.css";
 import { useUserAuth } from "../context/UserAuthContext";
 
 const PhoneSignUp = () => {
   const [error, setError] = useState("");
-  const [number, setNumber] = useState("");
+  const [number, setNumber] = useState("9547454031");
   const [flag, setFlag] = useState(false);
-  const [otp, setOtp] = useState("");
+  const [otp, setOtp] = useState("404040");
   const [result, setResult] = useState("");
+  const [loader, setLoader] = useState(false);
   const { setUpRecaptha } = useUserAuth();
+
   const navigate = useNavigate();
 
   const getOtp = async (e) => {
@@ -35,11 +38,13 @@ const PhoneSignUp = () => {
     // console.log("hi");
     if (otp === "" || otp === null) return;
     try {
+      setLoader(true);
       await result.confirm(otp);
       navigate("/home");
       // console.log("verify OTP");
     } catch (err) {
       setError(err.message);
+      setLoader(false);
     }
   };
 
@@ -53,13 +58,16 @@ const PhoneSignUp = () => {
         >
           <img
             className="w-8 h-8 mr-2"
-            src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
+            src={require("./logo.png")}
             alt="logo"
           />
           Heavy Farm
         </a>
-
-        <div className=" bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+        {loader ? <Loader /> : null}
+        <div
+          style={{ paddingBottom: "30px" }}
+          className=" bg-white  rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700"
+        >
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
               Log in With Phone Number
@@ -83,6 +91,7 @@ const PhoneSignUp = () => {
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder=""
                   required=""
+                  value={number}
                   onChange={(e) => setNumber(e.target.value)}
                 />
               </div>
@@ -91,13 +100,14 @@ const PhoneSignUp = () => {
                 {toggle ? (
                   <>
                     <input
-                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      className="bg-gray-50  mb-5 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                       placeholder="Enter OTP"
+                      value={otp}
                       // maxWidth={"40%"}
                       onChange={(e) => setOtp(e.target.value)}
                     ></input>
                     <button
-                      className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+                      className="focus:outline-none   text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
                       onClick={verifyOtp}
                     >
                       Submit
